a802ac569f946d2656ec5c5a3cab9e3db2238073
nachospreafico <nachospreafico06@gmail.com>
2023-03-06 11:57:46 +0100

    cambios post entrega 1
    elimine el componente Titulo y Logo de App.jsx y cree el componente Header en ./components, donde puse el <div> con el titulo y Logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import "./font/Ringbearer.ttf";
 import "./font/Aniron.ttf";
+import Header from "./components/Header.jsx";
 import NavBar from "./components/NavBar.jsx";
-import Imagen from "./assets/Logo.png";
 import ItemListcontainer from "./components/ItemListContainer.jsx";
 
 const App = () => {
   return (
     <div>
-      <div className="header">
-        <Logo />
-        <Titulo />
-      </div>
+      <Header />
       <NavBar />
       <ItemListcontainer
         greeting="Not all those who wander are lost"
@@ -19,14 +16,6 @@ const App = () => {
       />
     </div>
   );
-};
-
-const Titulo = () => {
-  return <h1 className="titulo">FRODO 'S BARGAINS</h1>;
-};
-
-const Logo = () => {
-  return <img src={Imagen} id="logo"></img>;
 };
 
 export default App;
